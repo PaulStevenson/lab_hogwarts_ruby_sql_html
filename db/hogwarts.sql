d608d1c96e4dd957fc0d089913aaa0c16c77ca80
Paul <rpmstevenson@gmail.com>
2018-05-03 11:12:52 +0100

    initial commit

---
+++ db/hogwarts.sql
@@ -0,0 +1,9 @@
+DROP TABLE hogwarts;
+CREATE TABLE  hogwarts (
+
+  id SERIAL4 PRIMARY KEY,
+  first_name varchar(255),
+  last_name varchar(255),
+  house varchar(255),
+  age INT2
+);
---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+require_relative('../models/horgwarts')
+
+student1 = Hogwarts.new({
+  'first_name' => 'Iain'
+  'last_name' => 'Rogerson'
+  'house' => 'Gryffindor'
+  'age' => 12
+})
+student2 = Hogwarts.new({
+  'first_name' => 'Paul'
+  'last_name' => 'Stevenson'
+  'house' => 'Slytherin'
+  'age' => 11
+})
+student3 = Hogwarts.new({
+  'first_name' => 'Ryan'
+  'last_name' => 'Gosling'
+  'house' => 'Hufflepuff'
+  'age' => 13
+})
+student4 = Hogwarts.new({
+  'first_name' => 'Jessica'
+  'last_name' => 'Chastain'
+  'house' => 'Ravenclaw'
+  'age' => 12
+})
